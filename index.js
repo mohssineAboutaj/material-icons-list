b7efc636fdcca8e0fcfe83d6b764fa7cd5a6a73f
mohssineAboutaj <mohssineaboutaj1995@gmail.com>
2022-09-07 12:29:55 +0100

    chore: refresh data icons

--- index.js
+++ index.js
@@ -9,15 +9,12 @@ const iconsList = Array.from(data, (ic) => ic.name);
 
 /**
  * @type Function
- *
  * @description Get list of icons filterd by a category
- *
  * @param {String} cat
- *
  * @returns Array[String]
  */
 function iconsListByCategory(cat) {
-  let list = [];
+  const list = [];
   data.forEach((icon) => {
     if (icon.categories.includes(cat)) {
       list.push(icon.name);
@@ -28,11 +25,8 @@ function iconsListByCategory(cat) {
 
 /**
  * @type Function
- *
  * @description Get list of icons filterd by a tag
- *
  * @param {String} tag
- *
  * @returns Array[String]
  */
 function iconsListByTag(tag) {
@@ -47,21 +41,19 @@ function iconsListByTag(tag) {
 
 /**
  * @type Function
- *
  * @description Get list of icons filterd by popularity
- *
  * @param {String} count
- * 
  * @param {Boolean} onlyNames
- *
  * @returns Array[Object] | Array[String]
  */
 function iconsListByPopularity(coubt = iconsLength, onlyNames = false) {
-  data
-    .sort((a, b) => {
-      return ((a.popularity < b.popularity) ? 1 : -1);
-    });
-  return onlyNames? data.slice(0, coubt - 1).map(a => a.name) : data.slice(0, coubt - 1);
+  data.sort((a, b) => {
+    return a.popularity < b.popularity ? 1 : -1;
+  });
+
+  return onlyNames
+    ? data.slice(0, coubt - 1).map((a) => a.name)
+    : data.slice(0, coubt - 1);
 }
 
 // export
